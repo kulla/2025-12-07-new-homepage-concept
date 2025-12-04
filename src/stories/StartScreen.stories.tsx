30786609a924d9346e75629403b69a412f953fa6
Stephan Kulla <git.mail@kulla.me>
2025-12-04 19:10:26 +0100

    wip: Start working on start screen

--- src/stories/StartScreen.stories.tsx
+++ src/stories/StartScreen.stories.tsx
@@ -14,8 +14,29 @@ export const Default: Story = {}
 
 function StartScreen() {
   return (
-    <div className="font-mono bg-white dark:bg-gray-900">
-      Start Screen Component
+    <div className="bg-white w-full min-h-screen border-2 flex items-center justify-center">
+      <div className="flex items-center gap-10">
+        <div className="flex-shrink-0">
+          <img
+            src="https://avatars.githubusercontent.com/u/1327215?v=4"
+            alt="Portrait of Kulla"
+            className="w-40 h-40 rounded-full object-cover"
+          />
+        </div>
+
+        <div>
+          <p className="text-3xl md:text-4xl font-semibold mb-2">
+            Hi there! <span className="inline-block">👋</span>
+          </p>
+          <p className="text-2xl md:text-3xl font-light">
+            I am Kulla,
+            <br />
+            <span className="font-normal">
+              software developer and educator …
+            </span>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
