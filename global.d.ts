a3b80b3b989491cce1f0f9e66e543a0b716a0fd5
Stephan Kulla <git.mail@kulla.me>
2025-12-06 15:51:19 +0100

    refactor: Remove @pixi/react

--- global.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Stage } from '@pixi/layers'
-import type { PixiReactElementProps } from '@pixi/react'
-
-declare module '@pixi/react' {
-  interface PixiElements {
-    stage: PixiReactElementProps<typeof Stage>
-  }
-}
