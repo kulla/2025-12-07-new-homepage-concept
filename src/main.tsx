67266fa0dd928338b229f20f14354c2af5f607e6
Stephan Kulla <git.mail@kulla.me>
2025-12-06 20:26:42 +0100

    feat: Add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import Home from './Home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
